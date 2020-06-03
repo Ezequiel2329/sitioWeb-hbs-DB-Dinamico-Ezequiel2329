bf78ae45557e446a09fa5bbaacd228db76aac072
Ezequiel2329 <65137090+Ezequiel2329@users.noreply.github.com>
2020-06-03 16:10:27 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+
+const PUERTO = process.env.PORT || 3000;
+
+//rutas
+let rutasPintores = require('./routes/pintor');
+
+//web y hbs
+const app = express();
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.use(express.static(__dirname + '/public'));
+
+//usar rutas
+app.use('/', rutasPintores)
+
+//conexion con mongo osi osi
+
+mongoose.Promise = global.Promise;
+const cadenamongo = 'mongodb+srv://ezequiel:MongooDB2329ezequiel@4everaezequiel-tkmn2.mongodb.net/Examen?retryWrites=true&w=majority'
+mongoose.connect(cadenamongo,{useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=>{
+        console.log('Conexión establecida :D');
+    }).catch(err=> console.log(err));
+
+//arrancar servidor web
+app.listen(PUERTO, ()=>{
+    console.log('Escuchando el puerto...')
+})
